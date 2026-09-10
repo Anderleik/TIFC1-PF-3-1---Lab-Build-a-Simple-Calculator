b0d519336bc1315eaf2fa6fa1a4441794acab9af
Anderleik <anderleik@gmail.com>
2026-09-10 16:09:14 -0500

    Primera parte

---
+++ main.py
@@ -0,0 +1,9 @@
+# CALCULADORA SENCILLA
+#Solicitamos los numeros al usuario
+num1 = float(input("Ingrese el primer número: "))
+num2 = float(input("Ingrese el segundo número: "))
+print(num1 + num2)
+
+#Extra CALCULADORA AVANZADA
+print("\n" + "="*30)
+print("CALCULADORA AVANZADA")
